cc3f145cc9d44aacc253e6a06e9e39be6acf3ba7
Pinxiang Wang <pwang25@ncsu.edu>
2022-08-29 14:46:53 -0400

    requirements&setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AGoodRepo',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Pinxiang Wang',
+    author_email='pwang25@ncsu.edu',
+    description='This is a test python project'
+)
