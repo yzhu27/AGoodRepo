48bc11cf0ec9ca9d0b15b94d27fa2e0d365ff183
yzhu27 <yhzhu27@gmail.com>
2022-08-29 15:00:02 -0400

    finish python part

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'AGoodRepo',
+    version = '1.0',
+    description = 'This is the HW1 of Group 7.',
+    author = 'Group 7', 
+    url = 'https://github.com/yzhu27/AGoodRepo',
+    packages = find_packages(),
+)
