d04210a434f01f028ef3a095173131154ba37f4d
yzhu27 <yhzhu27@gmail.com>
2022-09-07 22:37:20 -0400

    add test folder

---
+++ test/test.py
@@ -0,0 +1,15 @@
+from main import get_max, get_min, get_mean
+import numpy as np
+
+def test_max():
+    x = np.arange(5)
+    assert(get_max(x) == 4)
+
+def test_min():
+    x = [2, 4, 7, 3, 10]
+    assert(get_min(x) == 2)
+
+def test_mean():
+    x = np.ones(10)
+    assert(get_mean(x) == 1)
+    
